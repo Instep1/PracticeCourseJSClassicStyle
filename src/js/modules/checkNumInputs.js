4eebc40246ca90e44c58925171214386790540fa
Maksim <lvutinwork@gmail.com>
2023-05-11 14:13:35 +0300

    feat: Добавлен функционал отправки информации с модулей Доделан функционал отправки данных при заполнении нескольких форм на странице, с помощью стейта

---
+++ src/js/modules/checkNumInputs.js
@@ -0,0 +1,11 @@
+const checkNumInputs = (selector) => {
+    const checkInputs = document.querySelectorAll(selector)
+
+    checkInputs.forEach(item => {
+        item.addEventListener('input', () => {
+            item.value = item.value.replace(/\D/, '');
+        });
+    });
+};
+
+export default checkNumInputs;
